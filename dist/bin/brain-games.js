a7132307985661a647d2fcbb621bb2a2dff828ed
Simp-lexx <imlexx007@gmail.com>
2018-12-25 02:13:26 +1000

    npm published, installed local

---
+++ dist/bin/brain-games.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+"use strict";
+
+console.log('Welcome to the Brain Games!');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUVBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw2QkFBWiIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52IG5vZGVcblxuY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzIScpO1xuIl19
