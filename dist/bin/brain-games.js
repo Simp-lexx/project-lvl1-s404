83f7369f05a7b90a77f07ad48ed8b2bf03fe1bbe
Simp-lexx <imlexx007@gmail.com>
2018-12-26 01:10:19 +1000

    remove dist folder

--- dist/bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = require("..");
-
-// console.log('Welcome to the Brain Games!');
-(0, _.hi)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUNBO0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgeyBoaSB9IGZyb20gJy4uJztcbi8vIGNvbnNvbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyEnKTtcbmhpKCk7XG4iXX0=
