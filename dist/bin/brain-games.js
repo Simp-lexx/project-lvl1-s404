35d65c54fabdd7458af299a2c4eb48adc2d4cac2
Simp-lexx <imlexx007@gmail.com>
2018-12-26 00:49:13 +1000

    Error correction

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 "use strict";
 
-console.log('Welcome to the Brain Games!');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUVBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw2QkFBWiIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52IG5vZGVcblxuY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzIScpO1xuIl19
+var _ = require("..");
+
+// console.log('Welcome to the Brain Games!');
+(0, _.hi)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUNBO0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgeyBoaSB9IGZyb20gJy4uJztcbi8vIGNvbnNvbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyEnKTtcbmhpKCk7XG4iXX0=
