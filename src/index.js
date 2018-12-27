297557e294bdbd7aa0474faf970c29781c332183
Simp-lexx <imlexx007@gmail.com>
2018-12-28 00:44:48 +1000

    correct small bugs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const hi = () => {
   console.log('Welcome to the Brain Games!');
   const reqName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${reqName} !`);
+  console.log(`Hello ${reqName}!`);
 };
 export default hi;
